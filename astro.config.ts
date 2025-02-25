321d745cf53550992d931a35063b5c15ae4c7b4e
Charles Wang <zwcharl@gmail.com>
2025-02-24 23:18:28 -0500

    add react

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig, envField } from "astro/config";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
   env: {
     schema: {
-      TURSO_DATABASE_URL: envField.string({ context: "server", access: "secret", optional: false }),
-      TURSO_AUTH_TOKEN: envField.string({ context: "server", access: "secret", optional: false }),
+      TURSO_DATABASE_URL: envField.string({
+        context: "client",
+        access: "public",
+        optional: false,
+      }),
+      TURSO_AUTH_TOKEN: envField.string({
+        context: "client",
+        access: "public",
+        optional: false,
+      }),
     },
   },
+
+  integrations: [react()],
 });

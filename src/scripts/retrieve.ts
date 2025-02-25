1b9f72b4ba23b47dafd55e4ce146670b2bfc95f0
Charles Wang <zwcharl@gmail.com>
2025-02-25 00:03:17 -0500

    update retrieve script

--- src/scripts/retrieve.ts
+++ src/scripts/retrieve.ts
@@ -1,15 +1,28 @@
 import { join, resolve } from "node:path";
 
-import { assets } from "@/db/schema";
+import { assets, history, keywords } from "@/db/schema";
 import { db } from "@/db/turso";
 import { eq } from "drizzle-orm";
 
 const dir = resolve("./");
 const assetName = Bun.argv[2] as string;
 
-const result = await db.select().from(assets).where(eq(assets.name, assetName));
+const assetResult = await db.select().from(assets).where(eq(assets.name, assetName));
+const keywordsResult = await db.select().from(keywords).where(eq(keywords.assetName, assetName));
+const historyResult = await db
+  .select()
+  .from(history)
+  .where(eq(history.assetName, assetName))
+  .orderBy(history.timestamp);
 
-const asset = result[0]!;
+const asset = assetResult[0]!;
+
+console.log("RETRIEVAL SUCCESS, ASSET DETAILS");
+console.log(` - Asset name: ${asset.name}`);
+console.log(` - Keywords: ${keywordsResult.map((key) => key.keyword).join(", ")}`);
+console.log(` - Number of commits: ${historyResult.length}`);
+console.log(` - Original author: ${historyResult[0]!.author}`);
+console.log(` - Current version: ${historyResult[historyResult.length - 1]!.version}`);
 
 const thumbnailBlob = new Blob([asset.image], { type: "image/png" });
 await Bun.write("thumbnail.png", thumbnailBlob);
